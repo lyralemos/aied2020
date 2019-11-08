584460cb15b4875996cafd81d7c888193924e310
Alexandre Marinho <alexandre.marinho@nti.ufal.br>
2019-11-08 10:58:37 -0300

    push-dir config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "./docs"),
+    // outputDir: path.resolve(__dirname, "./docs"),
     publicPath: '/aied2020/',
     configureWebpack: {
         plugins: [
