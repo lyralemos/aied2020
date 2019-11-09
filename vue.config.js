0c54e8ec598f3de4ba25e52b81a36d7e30424989
Alexandre Marinho <lyralemos@gmail.com>
2019-11-09 08:42:55 -0300

    Disable custom publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
     // outputDir: path.resolve(__dirname, "./docs"),
-    publicPath: '/aied2020/',
+    // publicPath: '/aied2020/',
     configureWebpack: {
         plugins: [
             new webpack.ProvidePlugin({
