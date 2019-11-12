1de414ec7c6607db473546ea7219d84e6fec906c
Alexandre Marinho <alexandre.marinho@nti.ufal.br>
2019-11-12 10:32:41 -0300

    Dates page and favicon

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,20 +1,30 @@
 <template>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
+        <router-link :to="{name:'home'}" class="navbar-brand">
             <img src="../assets/aied2020-white.png" alt="AIED2020" title="" style="height: 3.8rem;">
-        </a>
+        </router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Dates</a>
-                </li>
+                <router-link
+                    :to="{ name: 'home' }"
+                    v-slot="{ href, route, navigate, isActive, isExactActive }"
+                    >
+                    <li :class=" ['nav-item', isExactActive && 'active']">
+                        <a class="nav-link" :href="href" @click="navigate">Home</a>
+                    </li>
+                </router-link>
+                <router-link
+                    to="/dates"
+                    v-slot="{ href, route, navigate, isActive, isExactActive }"
+                    >
+                    <li :class=" ['nav-item', isExactActive && 'active']">
+                        <a class="nav-link" :href="href" @click="navigate">Dates</a>
+                    </li>
+                </router-link>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Calls
---
+++ src/views/Dates.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="dates">
+        <section class="title">
+            <div class="container">
+                <div class="col-12">
+                    <h1 class="documentTitle">Important Dates</h1>
+                </div>
+            </div>
+        </section>
+
+        <section class="body">
+            <div class="container">
+                <div class="col-12">
+                    <h3>Submissions</h3>
+                    <ul>
+                        <li>Abstract for Full Papers & Short Papers: February 3, 2020 (11:59pm PST)</li>
+                        <li>Full and Short Papers: Feb 10, 2020</li>
+                        <li>Industry Papers & Innovation: Feb 17, 2020</li>
+                        <li>Workshop & Tutorial Proposals: Feb 17 , 2020</li>
+                        <li>Doctoral Consortium papers: Feb 24, 2020</li>
+                        <li>Interactive Events: April 24, 2020</li>
+                    </ul>
+
+                    <h3>Notification</h3>
+                    <ul>
+                        <li>Paper review (internal to reviewers): March 16, 2020</li>
+                        <li>Papers & Posters acceptance/rejection: March 30, 2020</li>
+                    </ul>
+                </div>
+            </div>
+            
+        </section>
+    </div>
+</template>
