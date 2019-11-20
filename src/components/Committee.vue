c8d5d58d83be3530f2ae9d97860b19574ebb241b
Alexandre Marinho <lyralemos@gmail.com>
2019-11-20 07:30:24 -0300

    Update General and Committee

--- src/components/Committee.vue
+++ src/components/Committee.vue
@@ -6,10 +6,23 @@
             <li>Rose Luckin. London Knowledge Lab, United Kingdom</li>
         </ul>
 
+        <h4>Advisory Board</h4>
+        
+        <ul>
+            <li>Danielle McNamara. Arizona State University</li>
+            <li>Ido Roll. Israel Institute of Technology</li>
+        </ul>
+        
         <h4>Local Organizing Chair</h4>
 
         <ul>
             <li>Violetta Cavalli-Sforza - Al Akhawayn University in Ifrane</li>
+        </ul>
+
+        <h4>Senior PC Chairs</h4>
+
+        <ul>
+            <li>Eva Millan. University of Málaga</li>
         </ul>
 
         <h4>Program co-chairs</h4>
@@ -17,9 +30,7 @@
         <ul>
             <li>Ig Ibert Bittencourt. Universidade Federal de Alagoas</li>
             <li>Mutlu Cukurova. University College London</li>
-            <!-- <li>Danielle McNamara. Arizona State University</li> -->
             <li>Kasia Muldner. Carleton University</li>
-            <!-- <li>Ido Roll. Israel Institute of Technology</li> -->
         </ul>
     </div>
 </template>
--- src/views/calls/General.vue
+++ src/views/calls/General.vue
@@ -12,17 +12,17 @@
             <div class="container">
                 <div class="col-12">
                     <p>
-                        The 2020 conference on Artificial Intelligence in Education will take place in Ifrane (Morocco), between July 7 and 10. 
+                        The 2019 conference on Artificial Intelligence in Education will take place in Ifrane (Morocco), between July 7 and 10.
                     </p>
                     <p>
-                        The AIED 2020 conference theme will be “Augmented Intelligence to Empower Education”.  As AI in Education systems get more 
-                        mature and are implemented at scale in real-world contexts, the value of using AI systems in the service of human decision 
-                        making, rather than automated personalisation, becomes more apparent than ever. While this paradigm of augmented intelligence 
-                        is not new to the field,solid theoretical and/or empirical work in the area is limited. Further work is needed to understand 
-                        the balance of human and artificial aspects in intelligence systems that involve human and AI partnerships. Developing and 
-                        implementing AI-human hybrid systems requires new research and many questions remain to be answered. In this conference, we 
-                        invite the community to think about intelligence augmentation opportunities that would empower key stakeholders of education 
-                        and provide innovative and creative solutions supported with empirical evidence.  
+                        The AIED 2020 conference theme will be “Augmented Intelligence to Empower Education”.  As AI in Education systems get 
+                        more mature and are implemented at scale in real-world contexts, the value of using AI systems in the service of human 
+                        decision making, rather than automated personalisation, becomes more apparent than ever. While this paradigm of augmented 
+                        intelligence is not new to the field,solid theoretical and/or empirical work in the area is limited. Further work is needed 
+                        to understand the balance of human and artificial aspects in intelligence systems that involve human and AI partnerships. 
+                        Developing and implementing AI-human hybrid systems requires new research and many questions remain to be answered. In this 
+                        conference, we invite the community to think about intelligence augmentation opportunities that would empower key stakeholders 
+                        of education and provide innovative and creative solutions supported with empirical evidence.
                     </p>
                     <p>
                         AIED 2020 will be the 21th edition of a longstanding series of international conferences, formerly bi-annual and now annual, 
@@ -38,37 +38,38 @@
                             Tangible interfaces, wearables and augmented reality.
                         </li>
                         <li>
-                            <b>Modelling and Representation:</b> Models of learners, including open learner models; facilitators, tasks and problem-solving processes; 
-                            Models of groups and communities for learning; Modelling motivation, metacognition, and affective aspects of learning; Ontological 
-                            modelling; Computational thinking and model-building; Representing and analyzing activity flow and discourse during learning.
+                            <b>Modelling and Representation:</b> Models of learners, including open learner models; facilitators, tasks and problem-solving 
+                            processes; Models of groups and communities for learning; Modelling motivation, metacognition, and affective aspects of learning; 
+                            Ontological modelling; Computational thinking and model-building; Representing and analyzing activity flow and discourse during 
+                            learning.
                         </li>
                         <li>
-                            <b>Models of Teaching and Learning:</b> Intelligent tutoring and scaffolding; Motivational diagnosis and feedback; Interactive pedagogical 
-                            agents and learning companions; Agents that promote metacognition, motivation and affect; Adaptive question-answering and dialogue, 
-                            Educational data mining, Learning analytics and teaching support, Learning with simulations
+                            <b>Models of Teaching and Learning:</b> Intelligent tutoring and scaffolding; Motivational diagnosis and feedback; Interactive 
+                            pedagogical agents and learning companions; Agents that promote metacognition, motivation and affect; Adaptive question-answering 
+                            and dialogue, Educational data mining, Learning analytics and teaching support, Learning with simulations
                         </li>
                         <li>
-                            <b>Learning Contexts and Informal Learning:</b> Educational games and gamification; Collaborative and group learning; Social networks; 
-                            Inquiry learning; Social dimensions of learning; Communities of practice; Ubiquitous learning environments; Learning through 
-                            construction and making; Learning grid; Lifelong, museum, out-of-school, and workplace learning.
+                            <b>Learning Contexts and Informal Learning:</b> Educational games and gamification; Collaborative and group learning; Social 
+                            networks; Inquiry learning; Social dimensions of learning; Communities of practice; Ubiquitous learning environments; Learning 
+                            through construction and making; Learning grid; Lifelong, museum, out-of-school, and workplace learning.
                         </li>
                         <li>
-                            <b>Evaluation:</b> Studies on human learning, cognition, affect, motivation, and attitudes; Design and formative studies of AIED systems; 
-                            Evaluation techniques relying on computational analyses.
+                            <b>Evaluation:</b> Studies on human learning, cognition, affect, motivation, and attitudes; Design and formative studies of 
+                            AIED systems; Evaluation techniques relying on computational analyses.
                         </li>
                         <li>
-                            <b>Innovative Applications:</b> Domain-specific learning applications (e.g. language, science, engineering, mathematics, medicine, military, 
-                            industry); Scaling up and large-scale deployment of AIED systems.
+                            <b>Innovative Applications:</b> Domain-specific learning applications (e.g. language, science, engineering, mathematics, 
+                            medicine, military, industry); Scaling up and large-scale deployment of AIED systems.
                         </li>
                         <li>
-                            <b>Intelligent techniques to support disadvantaged schools and students. Inequity and inequality in education:</b> socio-economic, gender, 
-                            and racial issues. Ethics in educational research: sponsorship, scientific validity, participant's rights and responsibilities, data 
-                            collection, management and dissemination.
+                            <b>Intelligent techniques to support disadvantaged schools and students. Inequity and inequality in education:</b> 
+                            socio-economic, gender, and racial issues. Ethics in educational research: sponsorship, scientific validity, participant's 
+                            rights and responsibilities, data collection, management and dissemination.
                         </li>
                         <li>
-                            Design, use, and evaluation of human-AI hybrid systems for learning; Research that explores the potential of human-AI interaction 
-                            in educational contexts; Systems and approaches in which educational stakeholders and AI tools build upon each other’s complementary 
-                            strengths to achieve educational outcomes and/or improve mutually. 
+                            Design, use, and evaluation of human-AI hybrid systems for learning; Research that explores the potential of human-AI 
+                            interaction in educational contexts; Systems and approaches in which educational stakeholders and AI tools build upon 
+                            each other’s complementary strengths to achieve educational outcomes and/or improve mutually. 
                         </li>
                     </ol>
 
@@ -87,26 +88,33 @@
 
                     <p>
                         Full papers, short papers, and industry papers will be published by Springer Lecture Notes in 
-                        Computer Science (LNCS). Submissions should be in Springer format. Papers that do not use the 
-                        required format might be rejected without review. For further details about the format, please 
-                        see  
+                        Computer Science (LNCS). Submissions must be in Springer format. Papers that do not use the 
+                        required format may be rejected without review. For further details about the format, please see  
                         <a href="https://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0" target="_blank">
                             https://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0
                         </a>.
                     </p>
 
                     <p>
-                        For the main track, there are two categories of papers. Full papers should present integrative 
-                        reviews or original reports of substantive new work: theoretical, empirical, and/or in the design, 
-                        development and/or deployment of novel concepts, systems, and mechanisms. Short papers are expected 
-                        to describe novel and interesting results to the overall community at large. The goal is to give 
-                        novel but not necessarily mature work a chance to be seen by other researchers and practitioners and 
-                        to be discussed at the conference. They will be presented as posters.
+                        For the main track, there are two categories of papers. Full papers should present integrative reviews 
+                        or original reports of substantive new work: theoretical, empirical, and/or in the design, development 
+                        and/or deployment of novel concepts, systems, and mechanisms. Full papers will be presented as talks. 
+                        Short papers are expected to describe novel and interesting results to the overall community at large. 
+                        The goal is to give novel but not necessarily mature work a chance to be seen by other researchers and 
+                        practitioners and to be discussed at the conference. Short papers  will be presented as posters.
                     </p>
 
                     <p>
                         All papers will be reviewed by the program committee to meet rigorous academic standards of publication. 
-                        Papers will be reviewed for relevance, novelty, technical soundness, significance and clarity of presentation. 
+                        The review process will be double-blind review process, meaning that both the authors and reviewers will 
+                        remain anonymous. To this end, authors should: (a) eliminate all information that could lead to their 
+                        identification (names, contact information, affiliations, patents, names of approaches, frameworks, 
+                        projects and/or systems); (b) cite to your prior work (if needed) in the third person; and (c) eliminate 
+                        acknowledgments and references to funding sources. Papers will be reviewed for relevance, novelty, 
+                        technical soundness, significance and clarity of presentation.
+                    </p>
+
+                    <p>
                         It is important to note that the work presented should not have been published previously or be under 
                         consideration in other conferences of journals. Any paper caught in double submission will be rejected 
                         without review.
