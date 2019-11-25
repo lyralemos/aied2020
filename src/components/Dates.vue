6f458f8e460496315494e46a03063399a5e1ecc9
Alexandre Marinho <lyralemos@gmail.com>
2019-11-25 09:06:55 -0300

    Dates updated

--- src/components/Dates.vue
+++ src/components/Dates.vue
@@ -1,15 +1,17 @@
 <template>
-    <ul>
-        <li>Abstract for Full Papers and Short Papers: Feb, 20, 2020 1:59pm PST</li>
-        <li>Full Papers, Short Papers Industry Papers: Feb 27, 2020, 11:59pm PST</li>
-        <li>Workshop & Tutorial Proposals: March 15, 2020, 11:59pm PST</li>
-        <li>Doctoral Consortium papers: March 15, 2020, 11:59pm PST</li>
-        <li>Interactive Events: April 29, 2020, 11:59pm PST</li>
-        <br />
+    <div>
+        <ul>
+            <li>Abstract for Full Papers and Short Papers: Feb, 20, 2020 1:59pm PST</li>
+            <li>Full Papers, Short Papers Industry Papers: Feb 27, 2020, 11:59pm PST</li>
+            <li>Workshop & Tutorial Proposals: March 15, 2020, 11:59pm PST</li>
+            <li>Doctoral Consortium papers: March 15, 2020, 11:59pm PST</li>
+            <li>Interactive Events: April 29, 2020, 11:59pm PST</li>
+        </ul>
+        <h5>Other Important Dates</h5>
         <ul>
             <li>Meta-reviews: April 8, 2020, 11:59pm PST</li>
             <li>Notification: April 22, 2020, 11:59pm PST</li>
-            <li>Camera-ready: May 6, 2020, 11:59pm PST</li>
+            <li>Camera-ready papers: May 6, 2020, 11:59pm PST</li>
         </ul>
-    </ul>
+    </div>
 </template>
