76582b05dc64847f7120f8d813bb1bc5beca38f7
Alexandre Marinho <lyralemos@gmail.com>
2019-12-12 22:14:52 -0300

    Chair updates

--- src/components/Committee.vue
+++ src/components/Committee.vue
@@ -5,13 +5,6 @@
         <ul>
             <li>Rose Luckin. London Knowledge Lab, United Kingdom</li>
         </ul>
-
-        <h4>Advisory Board</h4>
-        
-        <ul>
-            <li>Danielle McNamara. Arizona State University</li>
-            <li>Ido Roll. Technion Israel Institute of Technology</li>
-        </ul>
         
         <h4>Local Organizing Chair</h4>
 
@@ -32,5 +25,31 @@
             <li>Mutlu Cukurova. University College London</li>
             <li>Kasia Muldner. Carleton University</li>
         </ul>
+
+        <h4>Advisory Board</h4>
+        
+        <ul>
+            <li>Danielle McNamara. Arizona State University</li>
+            <li>Ido Roll. Technion Israel Institute of Technology</li>
+        </ul>
+
+        <h4>Workshop Chairs</h4>
+        <ul>
+            <li>Alexandra Cristea, Durham University, UK</li>
+            <li>Mingyu Feng, WestEd, USA</li>
+        </ul>
+        
+        <h4>Doctoral Consortium Chairs</h4>
+        <ul>
+            <li>Janice Gobert, Rutgers Graduate School of Education, USA</li>
+            <li>Porayska-Pomsta, Kaska k.porayska-pomsta@ucl.ac.uk, Knowledge Lab, UCL, London, UK</li>
+        </ul>
+        
+        <h4>Industry & Innovation Chairs</h4>
+        <ul>
+            <li>Elle Yuan Wang, ASU EdPlus, USA</li>
+            <li>Richard Tong and Dr. Wei Cui Squirrel AI, China</li>
+        </ul>
+        
     </div>
 </template>
