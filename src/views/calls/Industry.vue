a48b8110ee50f6fe86b51c9c3b098db297c95da3
Alexandre Marinho <lyralemos@gmail.com>
2020-02-14 17:08:41 -0300

    Call for industry papers

--- src/views/calls/Industry.vue
+++ src/views/calls/Industry.vue
@@ -11,7 +11,37 @@
         <section class="body">
             <div class="container">
                 <div class="col-12">
-                    TBA
+                    <h2>BRIDGING THE GAP BETWEEN RESEARCH AND INNOVATION</h2>
+                    <p>Intelligent, adaptive learning technologies that implement recent advances from AI are becoming more widespread in real-world use. However, this is still a challenge to enable AIED research to become an innovative solution to industry in order to impact various educational stakeholders (e.g. teachers, learners, parents, institutions). These educational technologies may be at different stages of development from prototypes being tested in academia or industry to deployed applications.</p>
+                    <p>For industry participants, this is an opportunity to learn about some of the most promising new developments in AIED, to present their findings, to connect with academic and industry partners and to recruit some of the most competent and creative talents in the field.</p>
+                    <p>For those in the research community, this is a chance to present their solutions/results and build connections that may result in the commercialization and wide distribution of new technologies. Papers will be presented as short presentations in the track session and as posters/interactive events in the main session.</p>
+                    <p>The Industry and Innovation Track is intended to support connections between industry (both for-profit and non-profit) and the research community and to share experiences about how to bridge the gap between research and innovation in the field of AI and education.</p>
+
+                    <h3>Evaluation Criteria</h3>
+                    <p>Papers will be reviewed by members of the Conference Program Committee. Criteria for selection include: appropriateness to the goals of the Industry and Innovation track, innovation, and potential impact. Submissions are expected to address the conference theme on “Augmented Intelligence to Empower Education”.</p>
+
+                    <h3>Form and Content</h3>
+                    <p>Accepted papers will be presented as short presentations in the industry and innovation track session and as posters/interactive events. We strongly recommend the authors to include a session about how and/or if they bridged the gap between research and practical innovation. In addition, some presenters will be invited to join an industry and innovation panel. The arrangements about the panel will be provided closer to the conference dates.</p>
+
+                    <h3>Submission Instructions</h3>
+                    <p>
+                        Please submit your paper at 
+                        <a href="https://easychair.org/conferences/?conf=aied2020" target="_blank">
+                            https://easychair.org/conferences/?conf=aied2020
+                        </a>
+                    </p>
+                    <p>
+                        Submissions must be in Springer format. Papers that do not use the required format may be rejected without review. For further details about the format, please see 
+                        <a href="https://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0" target="_blank">
+                            https://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0
+                        </a>.
+                    </p>
+                    <p>Proposals for this track should be 2-4 pages.</p>
+                    
+                    <h3>Important Dates</h3>
+                        <ul>
+                            <li>Industry Papers: Feb 27, 2020, 11:59pm PST</li>
+                        </ul>
                 </div>
             </div>
         </section>
