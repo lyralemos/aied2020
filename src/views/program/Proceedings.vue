00e7bd97132518c51625c166664525f365ed54be
Alexandre <lyralemos@gmail.com>
2020-07-05 09:33:03 -0300

    Proceddings view

--- src/views/program/Proceedings.vue
+++ src/views/program/Proceedings.vue
@@ -11,7 +11,16 @@
         <section class="body">
             <div class="container">
                 <div class="col-12">
-                    TBA
+                    <p>Free access until August 3rd, 2020</p>
+                    <div class="text-center">
+                        <a class="btn btn-light mr-3" href="https://link.springer.com/book/10.1007/978-3-030-52237-7" target="_blank">
+                            <img src="@/assets/part1.jpeg" />
+                        </a>
+                        
+                        <a class="btn btn-light" href="https://link.springer.com/book/10.1007/978-3-030-52240-7" target="_blank">
+                            <img src="@/assets/part2.jpeg" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
