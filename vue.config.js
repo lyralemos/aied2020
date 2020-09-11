9482e873f1d7fa8a19035bc4fabda622f2a98875
Alexandre <alexandre@MacBook-Air-de-Alexandre.local>
2020-09-11 15:12:10 -0300

    generic publicPath and version bump

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
     // outputDir: path.resolve(__dirname, "./docs"),
-    // publicPath: '/aied2020/',
+    publicPath: '',
     configureWebpack: {
         plugins: [
             new webpack.ProvidePlugin({
